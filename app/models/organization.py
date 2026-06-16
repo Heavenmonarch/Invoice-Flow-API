79f4903da9ace54ba834cd62cb6ac85ffeb91b86
heavenmonarch <heavenmonarc@gmail.com>
2026-06-16 22:21:46 +0100

    Added organization table

---
+++ app/models/organization.py
@@ -0,0 +1,17 @@
+import uuid
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+from app.core.database import Base
+from app.models.base import TimestampMixin
+
+
+class Organiztion(Base, TimestampMixin):
+    __tablename__= "organizations"
+    
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True, default=uuid.uuid4
+    )
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    slug: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
