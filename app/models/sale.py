7059f68b38aa8bdf54f83c6306cb7abc6acefb2b
heavenmonarch <heavenmonarc@gmail.com>
2026-06-17 10:23:44 +0100

    Added sale module

--- app/models/sale.py
+++ app/models/sale.py
@@ -8,4 +8,35 @@ from app.models.base import TimestampMixin
 class Sale(Base, TimestampMixin):
     __tablename__: "sales"
     
-    id: Maped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    
+    organization_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("organizations.id"), nullable=False
+    )
+    
+    staff_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("users.id"), nullable=False
+    )
+    
+    product_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("products.id"), nullable=False
+    )
+    
+    quantity: Mapped[int] = mapped_column (Integer, nullable=False)
+    
+    unit_price: Mapped[float] = mapped_column(Numeric(12,2), nullable=False)
+    
+    total_amount: Mapped[float] = mapped_column(Numeric(12,2), nullable=False)
+    
+    commission_rate: Mapped[float] = mapped_column(Numeric(5,2), nullable=False)
+    
+    commission_amount: Mapped[float] =mapped_column(Numeric(12,2), nullable=False)
+    
+    notes: Mapped[str] = mapped_column(String(500), nullable=True)
+    
+    
+    # relationships
+    
+    organization: Mapped["Organization"] = relationship()
+    staff: Mapped["User"] = relationship(back_populates="sales")
+    product: Mapped["Product"] = relationship(back_populates="Sales")
