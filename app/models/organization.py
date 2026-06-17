b7aaef9330288074c25e9c089e32c5ba4db4a7e8
heavenmonarch <heavenmonarc@gmail.com>
2026-06-17 09:28:50 +0100

    Created users model

--- app/models/organization.py
+++ app/models/organization.py
@@ -1,6 +1,6 @@
 import uuid
 from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.core.database import Base
 from app.models.base import TimestampMixin
 
@@ -15,3 +15,6 @@ class Organiztion(Base, TimestampMixin):
     slug: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    
+    # relationship definition
+    user: Mapped[list["User"]] = relationship(back_populates="organization")
---
+++ app/models/user.py
@@ -0,0 +1,36 @@
+import uuid
+from enum import Enum as PyEnum
+from sqlalchemy import String, Boolean, ForeignKey, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.core.database import Base
+from app.models.base import TimestampMixin
+
+
+class UserRole(str, PyEnum):
+    SUPERADMIN = "superadmin"
+    ADMIN = "admin"
+    STAFF = "staff"
+    
+
+class User (Base, TimestampMixin):
+    __tablename__: "users"
+    
+    id: Mapped[uuid.UUID] = mapped_column(ForeignKey("organizations.id"), nullable=False)
+    
+    organization_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("organizations.id"), nullable=False)
+    
+    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    
+    full_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    
+    password: Mapped[str] = mapped_column(String(50), nullable=False)
+    
+    role: Mapped[UserRole] = mapped_column(
+        Enum(UserRole), default=UserRole.STAFF,nullable=False,
+    )
+    
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    
+    
+    # relationship definiaions
+    organization: Mapped["Organization"] = relationship(back_populates="users")
