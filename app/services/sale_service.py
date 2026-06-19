d03c83b37736eba7b457c782338ff29eeb343a00
heavenmonarch <heavenmonarc@gmail.com>
2026-06-19 22:42:20 +0100

    started sale service

---
+++ app/services/sale_service.py
@@ -0,0 +1,39 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, func
+from fastapi import HTTPException, status
+from datetime import datetime, timezone
+import uuid
+
+from app.models.sale import Sale
+from app.models.product import Product
+from app.models.commission import Commission, CommissionStatus
+from app.models.user import User
+from app.schemas.sale import SaleCreate, SaleOut
+from app.schemas.common import PaginatedResponse
+
+
+class SaleService:
+    
+    @staticmethod
+    async def submit_sale(payload: SaleCreate, current_user: User, db: AsyncSession) -> Sale:
+        result = await db.execute(select(Product).where(Product.id == payload.product_id, Product.organization_id == current_user.organization_id, Product.is_active == True))
+        
+        product = result.scalar_one_or_none()
+        
+        if not product:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Product not found"
+            )
+            
+        unit_price = float(product.price)
+        commission_rate = float(product.commission_rate)
+        
+        total_amount = round(unit_price * payload.quantity, 2)
+        commission_amount = round(total_amount * (commission_rate/100), 2)
+        
+        now = datetime.now(timezone.utc)
+        period = now.strftime("%Y-%m")
+        
+        # creating the sale record after getting prerequired data
+        
